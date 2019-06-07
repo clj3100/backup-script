eab0affeefc1277db44b361e3e5180c41828836e
clj3100 <craig@cjonesflix.com>
2019-06-07 01:46:32 -0400

    Fixing multiupload after successfull tests Commenting debug echo's

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ back_jail="Backup"
 vaultname="backup-cjonesflix"
 backup_log="/var/log/backup.log"
 hash_loc=$back_loc/hash
-partsize="200M"
+partsize="128M"
 
 
 #Defining a debug step by step function
@@ -16,9 +16,9 @@ function verify {
 read -r -p "Continue? Y/N" yesno
 if [[ "$yesno" =~ ^([yY][eE][sS]|[yY])+$ ]]
     then
-	exit 1
-else
 	echo -n
+else
+	exit 1
 fi
 }
 
@@ -46,15 +46,6 @@ partsize=$( echo $partsize |sed -e 's/K/\*1024/g' -e 's/M/\*1048576/g' -e 's/G/\
 #echo $snapname
 #echo $filename
 
-#Checking for old backups to prune
-pruned=$(find $back_loc -maxdepth 1 -name "$jail*" -mtime +30 -print -type f -exec rm {} \; | sed "s:$back_loc/::" | tr "\n" " ")
-if [[ $(test -z "$pruned" ;echo $?) -eq 0 ]];
-    then
-	echo $(date '+%b %d %H:%M:%S') $(hostname -s) prune: Nothing to prune >> $backup_log
-else
-	echo $(date '+%b %d %H:%M:%S') $(hostname -s) prune: Pruned $pruned >> $backup_log
-fi
-
 #Adding line to backup log
 echo $(date '+%b %d %H:%M:%S') $(hostname -s) backup: Starting backup of jail $jail >> $backup_log
 
@@ -75,6 +66,7 @@ hash=$(jexec $(jid $back_jail) treehash $jail_back_loc/$filename)
 
 echo $hash > $hash_loc/$jail@$(date +%Y%m%d).hash
 
+i=0
 if [[ $filesize -gt 3500000000 ]]
     then
 	#Multipart Upload
@@ -82,32 +74,34 @@ if [[ $filesize -gt 3500000000 ]]
 	echo $(date '+%b %d %H:%M:%S') $(hostname -s) backup: Uploading to Glacier >> $backup_log
 	init=$(jexec $(jid $back_jail) aws glacier initiate-multipart-upload --account-id - --vault-name $vaultname --part-size $partsize --archive-description $jailpath)
 	uploadid=$(echo $init | jq -r .uploadId)
+	#echo $uploadid
 	#Making tempdir and splitting file
 	tmppath=$(TMPDIR=$back_loc mktemp -d)
 	tmpd=$(echo $tmppath | sed 's:.*/::')
-	(cd $tmppath && split -a3 -b$partsize $backloc/$filename part)
+	(cd $tmppath && split -a3 -b$partsize $back_loc/$filename part)
 	filelist=$(ls $tmppath |sort)
+	#echo $tmppath
 	#Uploading parts
 	for f in $filelist;
 	    do
-		if [[ $(stat -f %z $f) -lt $partsize ]]
+		if [[ $(stat -f %z $tmppath/$f) -lt $partsize ]]
 		     then
 			byteStart=$((i*partsize))
-			byteEnd=$filesize
+			byteEnd=$(($filesize-1))
 			file=$(echo $f | sed 's:.*/::')
-			jexec $(jid $back_jail) aws glacier upload-multipart-part --body $jail_back_loc/$tmpd/$file --range "'"'bytes '"$byteStart"'-'"$byteEnd"'/*'"'" --account-id - --vault-name $vaultname --upload-id $uploadId
+			checksum=$(jexec $(jid $back_jail) aws glacier upload-multipart-part --body $jail_back_loc/$tmpd/$file --range "bytes $byteStart-$byteEnd/*" --account-id - --vault-name $vaultname --upload-id $uploadid)
 		else
 			byteStart=$((i*partsize))
 			byteEnd=$((i*partsize+partsize-1))
 			#Getting filename for part
 			file=$(echo $f | sed 's:.*/::')
-			jexec $(jid $back_jail) aws glacier upload-multipart-part --body $jail_back_loc/$tmpd/$file --range "'"'bytes '"$byteStart"'-'"$byteEnd"'/*'"'" --account-id - --vault-name $vaultname --upload-id $uploadId 
+			checksum=$(jexec $(jid $back_jail) aws glacier upload-multipart-part --body $jail_back_loc/$tmpd/$file --range "bytes $byteStart-$byteEnd/*" --account-id - --vault-name $vaultname --upload-id $uploadid)
 			i=$(($i+1))
 		fi
 	done
 	#Finishing upload
 	result=$(jexec $(jid $back_jail) aws glacier complete-multipart-upload --account-id - --vault-name $vaultname --archive-size $filesize --checksum $hash --upload-id $uploadid)
-
+	rm -rf $tmppath
 
 else
 	#Uploading to Glacier
@@ -119,3 +113,12 @@ fi
 archiveId=$(echo $result | jq -r .archiveId)
 
 echo $(date '+%b %d %H:%M:%S') $(hostname -s) backup: Finished backup of jail $jail saved to $archiveId >> $backup_log
+
+#Checking for old backups to prune
+pruned=$(find $back_loc -maxdepth 1 -name "$jail*" -mtime +30 -print -type f -exec rm {} \; | sed "s:$back_loc/::" | tr "\n" " ")
+if [[ $(test -z "$pruned" ;echo $?) -eq 0 ]];
+    then
+        echo $(date '+%b %d %H:%M:%S') $(hostname -s) prune: Nothing to prune >> $backup_log
+else
+        echo $(date '+%b %d %H:%M:%S') $(hostname -s) prune: Pruned $pruned >> $backup_log
+fi
