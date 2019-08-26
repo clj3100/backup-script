8740a9f30a3b2acf4a7e68b740f2191662831fc0
clj3100 <craig@cjonesflix.com>
2019-08-26 01:00:50 -0400

    Adding days to exclude for loop

--- prune.sh
+++ prune.sh
@@ -11,3 +11,16 @@ function jid {
 jls | grep "$1" | awk '{print $1}'
 
 }
+
+#Generating the days to keep so everything else is removed
+
+#Array init
+dates=()
+
+#Loop gets the last day of each month and saves to array
+for m in $(seq 1 12);
+    do
+	dates+=($(date -j -v+1m -v-1d -f %m.%d $m.1 +%m/%d))
+done
+
+
