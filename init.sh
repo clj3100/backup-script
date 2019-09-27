f7a523c5cda775bc4250981c90a559608c3920c2
clj3100 <craig@cjonesflix.com>
2019-09-27 07:14:13 -0400

    Beginning of init to install and configure

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Script to setup all of the related backup scripts
+
+#Checking for iocage or warden to create backup jail
+checkiocage=$(iocage list ;echo $?)
+checkwarden=$(warden list ;echo $?)
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=10
+BACKTITLE="Initialization Script"
+
+localetc=/usr/local/etc
+
+mkdir -p $localetc/git
+git clone https://github.com/clj3100/backup-script $localetc/git
+ln -s $localetc/git/backup-script/back.conf $localetc/back.conf
+for file in $(find $localetc/git/backup-script -name '*.sh')
+    do
+	name=$(echo $file|sed 's/.sh//')
+	ln -s $file /usr/local/sbin/$name
+done
+
+#Asking for backup jail creation
+createbackjail=$(dialog --clear --backtitle "$BACKTITLE" --title "Create backup jail" --yesno "Do you want this script to create the backup jail?" $HEIGHT $WIDTH 2>&1 >/dev/tty ;echo $?)
+
+if [[ $checkiocage -eq 0 ]]
+    then
+	
