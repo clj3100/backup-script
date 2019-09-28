5ba7e0adf14f472043675ce0c4b06fef42a9cf0a
Craig Jones <craig@cljones.info>
2019-09-28 15:33:42 -0400

    Adding iocage creation section

--- init.sh
+++ init.sh
@@ -22,6 +22,12 @@ for file in $(find $localetc/git/backup-script -name '*.sh')
 	ln -s $file /usr/local/sbin/$name
 done
 
+if [[ $(test -e $localetc/back.conf;echo $?) -eq 0 ]]
+    then
+	#Running source to the back file to load jid and jpath functions
+	source $localetc/back.conf
+fi
+
 #Asking for backup jail creation
 createbackjail=$(dialog --clear --backtitle "$BACKTITLE" --title "Create backup jail" --yesno "Do you want this script to create the backup jail?" $HEIGHT $WIDTH 2>&1 >/dev/tty ;echo $?)
 
@@ -30,4 +36,20 @@ if [[ $createbackjail -eq 0 ]]
 	ip=$(dialog --clear --backtitle "$BACKTITLE" --title "IP for backup jail" --inputbox "What IP address for backup jail?" $HEIGHT $WIDTH 2>&1 >/dev/tty)
 	if [[ $checkiocage -eq 0 ]]
 	    then
-		iocage create -r LATEST -n Backup ip4_addr=$ip
+		iocage create -r LATEST -n Backup ip4_addr=$ip boot=on vnet=on
+		backjid=$(jid Backup)
+		#Need to run the command to install pkg in new jail cause recent new jails do not have it by default
+		jexec $backjid pkg -y install awscli ruby25-gems 
+		jexec $backjid gem install treehash
+		awsid=$(dialog --clear --backtitle "$BACKTITLE" --title "AWS CLI config" --inputbox "Enter your aws access id" $HEIGHT $WIDTH 2>&1 >/dev/tty)
+		awssecret=$(dialog --clear --backtitle "$BACKTITLE" --title "AWS CLI config" --inputbox "Enter your aws access secret" $HEIGHT $IWDTH 2>&1 >/dev/tty)
+		
+	elif [[ $checkwarden -eq 0 ]]
+	    then
+		#Need to create the warden jail creation section
+		echo -n
+	fi
+else
+	dialog --clear --backtitle "$BACKTITLE" --title "Info for backup jail" --infobox "The backup jail needs to have awscll installed as well as the treehash command for hash generation" $HEIGHT $WIDTH 2>&1 >/dev/tty
+	exit 0
+fi
